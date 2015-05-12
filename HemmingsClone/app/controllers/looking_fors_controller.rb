750f12bb4b69d575e50f1e163dcd4928e122730d
Thomas Jenkins <tjenkins52689@gmail.com>
2015-05-12 16:58:45 -0400

    prepare to push to heroku

--- HemmingsClone/app/controllers/looking_fors_controller.rb
+++ HemmingsClone/app/controllers/looking_fors_controller.rb
@@ -7,14 +7,16 @@ class LookingForsController < ApplicationController
   def create
     @request = current_user.new(looking_for_params)
     @request.vehicle_id = find_vehicle.id
+    if @request.save
+      redirect to looking_for
   end
 
   def show
-
+    @request = LookingFor.includes(:vehicle).find(params[:id])
   end
 
   def index
-
+    @requests = current_user.looking_fors.includes(:vehicles)
   end
 
   def looking_for_params
--- HemmingsClone/config/routes.rb
+++ HemmingsClone/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  resources :users
+  resources :users, only: [:new, :create]
   resource :session, only: [:create, :new, :destroy]
   resources :vehicle_sales, only: [:new, :create,:index, :show]
   resources :part_sales, only: [:new, :create, :index, :show]
+  resources :looking_fors, only: [:new, :create, :index, :show]
 end
