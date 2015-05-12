fcef749b944ea6fe3deaccaa4114b3b17d8ef936
Thomas Jenkins <tjenkins52689@gmail.com>
2015-05-12 17:33:27 -0400

    fix stupid typos

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root: "sesssions#new"
+  root to: "sessions#new"
   resources :users, only: [:new, :create]
   resource :session, only: [:create, :new, :destroy]
   resources :vehicle_sales, only: [:new, :create,:index, :show]
