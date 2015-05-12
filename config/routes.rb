92dddeed1b86d5189528c655097a7e50592730fa
Thomas Jenkins <tjenkins52689@gmail.com>
2015-05-12 17:28:49 -0400

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root: "sesssions#new"
   resources :users, only: [:new, :create]
   resource :session, only: [:create, :new, :destroy]
   resources :vehicle_sales, only: [:new, :create,:index, :show]
