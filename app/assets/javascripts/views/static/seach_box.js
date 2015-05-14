775499c4a2a6027264c66e0f2756120380c975ee
arcane52689 <tjenkins52689@gmail.com>
2015-05-14 11:32:04 -0400

    add some style to search-form, add jquery selectors to determine whether searching for parts or vehicles

--- app/assets/javascripts/views/static/seach_box.js
+++ app/assets/javascripts/views/static/seach_box.js
@@ -2,8 +2,12 @@ HemmingsClone.Views.SearchBox = Backbone.CompositeView.extend({
   initialize: function(options) {
 
   },
+  tagName: "section",
+  className: "search",
+
   events: {
-    "submit form": "search"
+    "submit form": "search",
+    "click .search-option": "select"
   },
 
   template: JST["static/search"],
@@ -17,5 +21,19 @@ HemmingsClone.Views.SearchBox = Backbone.CompositeView.extend({
     event.preventDefault();
     var data = $(event.currentTarget).serializeJSON();
     console.log(data);
+  },
+
+  select: function(event) {
+    event.preventDefault();
+    $(".search-option").removeClass('selected')
+    var $li = $(event.currentTarget);
+    if ($li.attr("id") === "part_sales") {
+      $("#part-search").removeClass("inactive")
+    }
+    else {
+      $("#part-search").removeClass("inactive").addClass("inactive")
+    }
+    $li.addClass("selected");
   }
+
 })
