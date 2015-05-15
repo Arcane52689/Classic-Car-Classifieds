422705c9b30928677883eb1fcca7c115deb965da
arcane52689 <tjenkins52869@gmail.com>
2015-05-15 15:49:17 -0400

    stupid typo

--- app/models/part_tagging.rb
+++ app/models/part_tagging.rb
@@ -2,7 +2,7 @@ class PartTagging < ActiveRecord::Base
   belongs_to :vehicle
   validates :part_number, :vehicle_id, presence: true
 
-  has_many: :part_sales,
+  has_many :part_sales,
     class_name: "PartSale",
     foreign_key: :part_number,
     primary_key: :part_number
