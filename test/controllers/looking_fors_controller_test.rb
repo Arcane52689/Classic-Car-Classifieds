836891728da1169a257ed5f6542ed2ad619c6545
App Academy Student <appacademy@Apps-Mac-mini-6.local>
2015-05-22 09:09:13 -0400

    remove test folder'

--- test/controllers/looking_fors_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LookingForsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/part_tagging_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PartTaggingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
