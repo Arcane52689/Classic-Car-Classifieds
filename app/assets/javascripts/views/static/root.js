7447c3e3ffd4ae81f2e02ad5b0b68283e3835641
Thomas Jenkins <tjenkins52689@gmail.com>
2015-05-30 15:44:49 -0400

    More CSS

--- app/assets/javascripts/views/static/root.js
+++ app/assets/javascripts/views/static/root.js
@@ -2,18 +2,9 @@ HemmingsClone.Views.RootView = Backbone.CompositeView.extend({
   tagName: "article",
   className: "root",
   template: JST["static/root"],
-  events: {
-    "click li":"visitLink"
-  },
 
   render: function() {
     this.$el.html(this.template());
     return this;
   },
-
-  visitLink: function(event) {
-    event.preventDefault();
-    var url = $(event.currentTarget).find("a").attr("href");
-    Backbone.history.navigate(url, {trigger: true})
-  }
 })
